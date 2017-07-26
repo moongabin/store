a110fce4c380feee3d606fbe6089a153cb374ee4
moongabin <30468361+moongabin@users.noreply.github.com>
2017-07-26 22:23:11 +0900

    Add files via upload

---
+++ CallPlus.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Declare.h"
+
+int main()
+{
+
+	int a=33;
+	int b=27;
+	printf("%d + %d = %d\n",a,b,Plus(a,b));
+}
---
+++ Declare.h
@@ -0,0 +1,1 @@
+extern "C" _declspec(dllimport) int Plus(int, int);
